3f1220babddd49d87f3067add52b1cd33a747421
root <root@picoctf>
2026-05-24 22:05:51 +0700

    fondasi header

--- fondasi.c
+++ fondasi.c
@@ -1,1 +1,66 @@
-tes
+#include "header.h"
+
+/* =========================
+   INISIALISASI
+========================= */
+void initLinkedList(LinkedList *list) {
+    list->head = NULL;
+}
+
+void initHashTable(HashTable *ht) {
+    for(int i = 0; i < SIZE; i++) {
+        ht->table[i] = NULL;
+    }
+}
+
+void initStack(Stack *s) {
+    s->top = NULL;
+}
+
+void initQueue(Queue *q) {
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+/* =========================
+   FREE MEMORY (Pencegah Leak)
+========================= */
+void freeLinkedList(LinkedList *list) {
+    Node *temp;
+    while(list->head != NULL) {
+        temp = list->head;
+        list->head = list->head->next;
+        free(temp);
+    }
+}
+
+void freeHashTable(HashTable *ht) {
+    HashNode *temp, *curr;
+    for(int i = 0; i < SIZE; i++) {
+        curr = ht->table[i];
+        while(curr != NULL) {
+            temp = curr;
+            curr = curr->next;
+            free(temp);
+        }
+    }
+}
+
+void freeStack(Stack *s) {
+    StackNode *temp;
+    while(s->top != NULL) {
+        temp = s->top;
+        s->top = s->top->next;
+        free(temp);
+    }
+}
+
+void freeQueue(Queue *q) {
+    QueueNode *temp;
+    while(q->front != NULL) {
+        temp = q->front;
+        q->front = q->front->next;
+        free(temp);
+    }
+}
+
--- header.h
+++ header.h
@@ -1,1 +1,109 @@
-testes
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 10
+
+//struct barang
+typedef struct {
+    int id;
+    char nama[50];
+    char kategori[30];
+    int stok;
+    float harga;
+    int stokMinimum;
+    int totalTerjual;
+} Barang;
+
+//linked list (datautama)
+typedef struct Node {
+    Barang data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *head;
+} LinkedList;
+
+/* =========================
+   HASH TABLE
+========================= */
+typedef struct HashNode {
+    int id;
+    Node *barangPtr;
+    struct HashNode *next;
+} HashNode;
+
+typedef struct {
+    HashNode *table[SIZE];
+} HashTable;
+
+/* =========================
+   STACK (RIWAYAT)
+========================= */
+typedef struct StackNode {
+    Barang data;
+    struct StackNode *next;
+} StackNode;
+
+typedef struct {
+    StackNode *top;
+} Stack;
+
+/* =========================
+   QUEUE (TRANSAKSI)
+========================= */
+typedef struct QueueNode {
+    Barang data;
+    struct QueueNode *next;
+} QueueNode;
+
+typedef struct {
+    QueueNode *front;
+    QueueNode *rear;
+} Queue;
+
+/* =========================
+   PROTOTIPE FUNGSI WAJIB
+========================= */
+
+// Inisialisasi
+void initLinkedList(LinkedList *list);
+void initHashTable(HashTable *ht);
+void initStack(Stack *s);
+void initQueue(Queue *q);
+void mergeSort(Node **headRef);
+void tampilRanking(LinkedList *list);
+void identifikasiProduk(LinkedList *list);
+void tambahBarang(LinkedList *list,Queue *q,HashTable *ht);
+void tampilBarang(LinkedList *list);
+void editBarang(LinkedList *list, Queue *q);
+void hapusBarang(LinkedList *list);
+void cekStokMinimum(Barang barang, Queue *q);
+void enqueueRestock(Queue *q, Barang barang);
+void tampilQueueRestock(Queue *q);
+void pushStack(Stack *s, Barang data);
+int popStack(Stack *s, Barang *data);
+void transaksiPenjualan(LinkedList *list, Stack *riwayat, Queue *q);
+void undoTransaksi(LinkedList *list, Stack *riwayat);
+void tampilRiwayatTransaksi(Stack *riwayat);
+double hitungTotalNilaiInventori(LinkedList *list);
+void tampilkanLaporanInventori(LinkedList *list);
+
+int hashFunction(int id);
+void insertHash(HashTable *ht, int id, Node *barangPtr);
+Node* cariBarangByID(HashTable *ht, int id);
+Node* cariBarangByNama(LinkedList *list, const char *namaDicari);
+Barang dequeueRestock(Queue *q);
+void prosesRestock(LinkedList *list, Queue *q);
+// Free Memory
+void freeLinkedList(LinkedList *list);
+void freeHashTable(HashTable *ht);
+void freeStack(Stack *s);
+void freeQueue(Queue *q);
+
+
+#endif
